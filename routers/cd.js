0c914077658fe99cc5b7f7be427803176c2a3cf5
JANNIS-PASSALIS <PASALIS13@HOTMAIL.COM>
2020-11-26 14:41:09 +0100

    Add list and user in cd router

--- routers/cd.js
+++ routers/cd.js
@@ -1,11 +1,15 @@
 const { Router } = require("express");
 const Cd = require("../models").cd;
+const List = require("../models").list;
+const User = require("../models").user;
 
 const router = new Router();
 
 router.get("/", async (req, res, next) => {
   try {
-    const cds = await Cd.findAll();
+    const cds = await Cd.findAll({
+      include: { model: List, include: { model: User } },
+    });
     res.json(cds);
   } catch (e) {
     next(e);
