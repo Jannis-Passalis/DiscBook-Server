7fd05afc59bc5f9be437ec4f8e0aa7001d8a2b2c
JANNIS-PASSALIS <PASALIS13@HOTMAIL.COM>
2020-11-27 14:30:57 +0100

    Create router to add cds to database

--- routers/cd.js
+++ routers/cd.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const Cd = require("../models").cd;
 const List = require("../models").list;
 const User = require("../models").user;
+const authMiddleware = require("../auth/middleware");
 
 const router = new Router();
 
@@ -11,6 +12,29 @@ router.get("/", async (req, res, next) => {
       include: { model: List, include: { model: User } },
     });
     res.json(cds);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/add", authMiddleware, async (req, res, next) => {
+  // const { album, year, cdCover, userId } = req.body;
+  const artist = req.body.artist ? req.body.artist : null;
+  const album = req.body.album ? req.body.album : null;
+  const year = req.body.year ? req.body.year : null;
+  const cdCover = req.body.cdCover ? req.body.cdCover : null;
+  const userId = req.body.userId ? req.body.userId : null;
+
+  try {
+    const newCd = await Cd.create({
+      artist,
+      album,
+      releaseYear: year,
+      albumCover: cdCover,
+      listId: userId,
+      forSale: false,
+    });
+    res.json(newCd);
   } catch (e) {
     next(e);
   }
